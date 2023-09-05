012ff2125fb6bf7df5bc76f801da4c87763fe85b
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-09-05 15:18:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// script.js
+// const line = document.querySelector('.line');
+// let animationRunning = true;
+
+// line.addEventListener('click', () => {
+//     if (animationRunning) {
+//         line.style.animationPlayState = 'paused';
+//     } else {
+//         line.style.animationPlayState = 'running';
+//     }
+//     animationRunning = !animationRunning;
+// });
